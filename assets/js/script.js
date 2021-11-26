2fa62a2ffc37d8336fca0bda0ad1a760ee1cb959
austin espinal <austinespi@gmail.com>
2021-11-26 00:44:07 -0500

    add js variables and saved city array. made empty current city weather function. to filled later

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var city = "";
+//saved city array
+var savedCity = [];
+
+var searchCity = $("#search-city");
+var searchButton = $("#search-btn");
+var currentCity = $("#current-city");
+var temp = $("#temperature");
+var humidty= $("#humidity");
+var windSpeed=$("#wind-speed");
+var uvIndex= $("#uv-index");
+
+function currentCity() {
+
+}
