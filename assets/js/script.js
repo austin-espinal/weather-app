d5c89747c84553cbd21327a000b91114d5d020a0
austin espinal <austinespi@gmail.com>
2021-11-28 14:09:28 -0500

    obtain API key and working on fetching API response

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,22 @@ var humidty= $("#humidity");
 var windSpeed=$("#wind-speed");
 var uvIndex= $("#uv-index");
 
-function currentCity() {
+//one call API URL and key
+const apiKey = "2a2063c48ee95fac2d9d7192efde7145";
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID="+ apiKey;
 
-}
+function currentCity() {
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        } else {
+            alert("Error: City not found");
+        }
+    })
+    .catch(function (error) {
+        //Notice this .cath()' gettineg chained onto the end of the '.then()' method
+        alert("Unable to connect to One Call API");
+    });
+};
