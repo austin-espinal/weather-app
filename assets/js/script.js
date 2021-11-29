0351ed133622f03f9d053aa276d324cda9c45b7f
austin espinal <austinespi@gmail.com>
2021-11-28 23:46:40 -0500

    working on fetching API response and display weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,15 +10,23 @@ var humidty= $("#humidity");
 var windSpeed=$("#wind-speed");
 var uvIndex= $("#uv-index");
 
+//using LUXON for date
+var DateTime = luxon.DateTime;
+const now = DateTime.now();
+// $("#currentDay").text(now.toLocaleString());
+
 //one call API URL and key
 const apiKey = "2a2063c48ee95fac2d9d7192efde7145";
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID="+ apiKey;
 
-function currentCity() {
+
+
+function currentCity(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID="+ apiKey;
+
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                console.log(data);
+                displayWeather(data, city);
             });
         } else {
             alert("Error: City not found");
@@ -29,3 +37,7 @@ function currentCity() {
         alert("Unable to connect to One Call API");
     });
 };
+function displayWeather(){
+  
+}
+
